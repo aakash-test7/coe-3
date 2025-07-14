901eb061f9161175deacd381c91f675678cde498
Aakash Kharb <akgdsc7@gmail.com>
2025-07-14 15:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-st.set_page_config(page_title="CicerOmicsExplorer", layout="wide",initial_sidebar_state="expanded")
+st.set_page_config(page_title="CicerOmicsExplorer", layout="wide",initial_sidebar_state="collapsed")
 from streamlit_navigation_bar import st_navbar
 import pages as pg
 import time
-from pages.security_login import basic_stats, update_visitor_count
+from pages.mainapp import basic_stats, update_visitor_count
 
 pages = ["HOME", "SEARCH", "GENE-INFO", "SPATIAL-EXPRESSION", "RNA", "miRNA-Target", "PPI", "LOCALIZATION", "GO-KEGG", "SNP-CALLING", "ORTHOLOGS/PARALOGS", "ABOUT-US", "LOGIN"]
 logo_path = ("logo.svg")
